686dbc5bba1ede2956d0b38035bf70dca111527e
Shivamingale3 <shivamingale3@gmail.com>
2026-03-19 11:48:19 +0530

    feat: Add new types for user personal, professional, education, and scientific interest details.

--- src/types/user/user.types.ts
+++ src/types/user/user.types.ts
@@ -36,3 +36,80 @@ export type UserEducation = {
   order: number;
   created_at: string;
 };
+
+export type UserPersonalDetails = {
+  id: number;
+  biosketch: string | null;
+  research_links: string | null;
+  x_handle: string | null;
+  linkedin: string | null;
+  city: string | null;
+  country: string | null;
+  gender: string | null;
+  dob: string | null;
+  articles_journals: string | null;
+  book_chapters: string | null;
+} | null;
+
+export type UserProfessionalDetails = {
+  current_role: string | null;
+  current_organization: string | null;
+  current_department: string | null;
+  current_start_month: number | null;
+  current_start_year: number | null;
+  current_description: string | null;
+  work_email: string | null;
+  contact_number: string | null;
+  emergency_contact_number: string | null;
+  website: string | null;
+  lab: string | null;
+  work_address: string | null;
+  skill_set: string | null;
+  languages_spoken: string | null;
+  certifications: string | null;
+  past_experiences: UserPastProfessionalDetails[] | null;
+};
+
+export type UserPastProfessionalDetails = {
+  id: number;
+  role: string | null;
+  organization: string | null;
+  department: string | null;
+  start_month: number | null;
+  start_year: number | null;
+  end_month: number | null;
+  end_year: number | null;
+  description: string | null;
+};
+
+export type UserEducationDetails = {
+  id: number;
+  degree: string | null;
+  course_name: string | null;
+  specialization: string | null;
+  university: string | null;
+  institute: string | null;
+  place: string | null;
+  country: string | null;
+  start_year: number | null;
+  end_year: number | null;
+  is_current: boolean;
+  topic: string | null;
+  lab_or_department: string | null;
+  research_interests: string | null;
+  research_summary: string | null;
+  order: number;
+  created_at: string;
+}[];
+
+export type UserScientificInterest = {
+  id: number;
+  research_area_of_expertise: string | null;
+  major_focus: string[];
+  specific_research_areas: string[];
+  organ_sites: string[];
+  additional_research_areas: string[];
+  brief_description: string | null;
+  created_at: string;
+  updated_at: string;
+};
