396fde21a49f468faf570f94926b08198a56ea73
arunkhairwar2 <arun.khairwar@leadows.com>
2026-04-06 17:08:20 +0530

    [BUG-FIX]: Bug-32 password validation

--- src/validations/auth/register.ts
+++ src/validations/auth/register.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+export const passwordValidation = z
+  .string()
+  .min(8, "Password must be at least 8 characters")
+  .max(16, "Password must be at most 16 characters")
+  .regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[^\s]+$/,
+    "Password must include uppercase, lowercase, number, and special character",
+  );
+
 export const registerSchema = z
   .object({
     first_name: z
@@ -19,29 +28,15 @@ export const registerSchema = z
       .trim()
       .max(200, "Maximum 200 character")
       .toLowerCase(),
-    password: z
-      .string()
-      .min(8, "Password must be atleast 8 characters")
-      .max(16, "Password must be atmost 16 characters")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      ),
-    confirm_password: z
-      .string()
-      .min(8, "Password must be atleast 8 characters")
-      .max(16, "Password must be atmost 16 characters")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-      ),
+    password: passwordValidation,
+    confirm_password: z.string(),
     termsAccepted: z.boolean().refine((val) => val === true, {
       message: "You must accept the terms",
     }),
   })
   .refine((data) => data.password === data.confirm_password, {
     message: "Passwords do not match",
-    path: ["confirmPassword"],
+    path: ["confirm_password"],
   });
 
 export type RegisterForm = z.infer<typeof registerSchema>;
